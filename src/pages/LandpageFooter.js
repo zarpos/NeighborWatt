2a4d5251d88261f7ea09d317a76d1199430d6f03
Fede <federicojose2000@gmail.com>
2023-10-07 18:09:21 +0200

    cambios footer

--- src/pages/LandpageFooter.js
+++ src/pages/LandpageFooter.js
@@ -5,18 +5,18 @@ const LandpageFooter = () => {
         <div className={styles.landpageFooter}>
             <div className={styles.capturaDePantalla20231006} />
             <div className={styles.landpageFooterChild} />
-            <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+            <img className={styles.image2Icon} alt="Capa fondo" src="/image-2@2x.png" />
             <div className={styles.landpageFooterItem} />
             <b className={styles.yourInboxDeserves}>Your Inbox Deserves a Boost!</b>
             <b className={styles.subscribeToThe}>Subscribe to the WATTLetter</b>
             <div className={styles.landpageFooterInner} />
             <div className={styles.rectangleDiv} />
-            <b className={styles.subscribe}>Subscribe</b>
+            <a href="#" className={styles.subscribe}>Subscribe</a>
             <div className={styles.enterEmail}>Enter email</div>
             <div className={styles.allRightsReserved}>
                 © All rights reserved crypto Corporation
             </div>            
-            <img className={styles.nw1Icon} alt="" src="/nw-1@2x.png" />
+            <img className={styles.nw1Icon} alt="Logo footer" src="/nw-1@2x.png" />
             <div
                 className={styles.faqsContact}
             >
