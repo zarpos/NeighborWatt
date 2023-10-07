c7c6aebe52f20847a70b7e964a1d03c3289108a4
Fede <federicojose2000@gmail.com>
2023-10-07 17:57:40 +0200

    Landpage4 alts updated

--- src/pages/Landpage4.js
+++ src/pages/Landpage4.js
@@ -3,9 +3,9 @@ import styles from "./Landpage4.module.css";
 const Landpage4 = () => {
     return (
         <div className={styles.landpage4}>
-            <img className={styles.phone2Icon} alt="" src="/phone-2@2x.png" />
+            <img className={styles.phone2Icon} alt="Movil parte 2" src="/phone-2@2x.png" />
             <div className={styles.capturaDePantalla20231006} />
-            <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+            <img className={styles.image2Icon} alt="Capa fondo" src="/image-2@2x.png" />
             <div className={styles.landpage4Child} />
             <div className={styles.landpage4Item} />
             <b className={styles.b}>70%</b>
@@ -17,25 +17,25 @@ const Landpage4 = () => {
             <div className={styles.kgOfCo2}>Kg of CO2 reduced</div>
             <div className={styles.homesInSolar}>Homes in Solar Flow</div>
             <div className={styles.landpage4Inner} />
-            <img className={styles.lineIcon} alt="" src="/line-2.svg" />
-            <img className={styles.landpage4Child1} alt="" src="/line-3.svg" />
-            <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+            <img className={styles.lineIcon} alt="patch" src="/line-2.svg" />
+            <img className={styles.landpage4Child1} alt="patch" src="/line-3.svg" />
+            <img className={styles.image4Icon} alt="Imagen mapa" src="/image-4@2x.png" />
             <div className={styles.ellipseDiv} />
             <div className={styles.landpage4Child2} />
             <div className={styles.landpage4Child3} />
-            <img className={styles.vectorIcon} alt="" src="/vector-1.svg" />
-            <img className={styles.landpage4Child4} alt="" src="/vector-2.svg" />
-            <img className={styles.landpage4Child5} alt="" src="/vector-3.svg" />
-            <img className={styles.landpage4Child6} alt="" src="/vector-4.svg" />
-            <img className={styles.landpage4Child7} alt="" src="/vector-5.svg" />
-            <img className={styles.landpage4Child8} alt="" src="/vector-6.svg" />
-            <img className={styles.landpage4Child9} alt="" src="/vector-7.svg" />
-            <img className={styles.landpage4Child10} alt="" src="/vector-8.svg" />
-            <img className={styles.landpage4Child11} alt="" src="/vector-9.svg" />
-            <img className={styles.landpage4Child12} alt="" src="/vector-10.svg" />
-            <img className={styles.landpage4Child13} alt="" src="/vector-11.svg" />
-            <img className={styles.landpage4Child14} alt="" src="/vector-12.svg" />
-            <img className={styles.landpage4Child15} alt="" src="/vector-13.svg" />
+            <img className={styles.vectorIcon} alt="patch" src="/vector-1.svg" />
+            <img className={styles.landpage4Child4} alt="patch" src="/vector-2.svg" />
+            <img className={styles.landpage4Child5} alt="patch" src="/vector-3.svg" />
+            <img className={styles.landpage4Child6} alt="patch" src="/vector-4.svg" />
+            <img className={styles.landpage4Child7} alt="patch" src="/vector-5.svg" />
+            <img className={styles.landpage4Child8} alt="patch" src="/vector-6.svg" />
+            <img className={styles.landpage4Child9} alt="patch" src="/vector-7.svg" />
+            <img className={styles.landpage4Child10} alt="patch" src="/vector-8.svg" />
+            <img className={styles.landpage4Child11} alt="patch" src="/vector-9.svg" />
+            <img className={styles.landpage4Child12} alt="patch" src="/vector-10.svg" />
+            <img className={styles.landpage4Child13} alt="patch" src="/vector-11.svg" />
+            <img className={styles.landpage4Child14} alt="patch" src="/vector-12.svg" />
+            <img className={styles.landpage4Child15} alt="patch" src="/vector-13.svg" />
         </div>
     );
 };
